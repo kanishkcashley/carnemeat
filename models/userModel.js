a5d995236d5f675e26b6a591ae1572ed333575a6
kanishkcashley <147134788+kanishkcashley@users.noreply.github.com>
2024-02-24 22:23:31 +0530

    final code

---
+++ models/userModel.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please enter a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: [6, "Password must be up to 6 characters"],
+    },
+    phone: {
+      type: String,
+      required: true,
+      default: "+91",
+    },
+    address: {
+      type: {},
+      required: true,
+    },
+    answer: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("users", userSchema);
